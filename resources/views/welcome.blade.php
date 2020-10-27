bd79596c8936b3f6de501fb82a357b76b5cfee65
Kevin <kevin.gonzalez@creategicalatina.com>
2020-10-27 18:02:47 -0500

    pagina home terminada 27/10/2020

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,2 +1,25 @@
 @extends('layout.general')
 
+<div class="main-container">
+
+        <div class="login">
+            <form action="#">
+                <h2>Identifíquese</h2>
+                <p>¡El tiempo es cerebro!</p>
+
+                <label>*Obligatorio</label>
+                <input type="text" name="correo" id="correo" placeholder="Email*">
+                <input type="text" name="usuario" id="usuario" placeholder="Username*">
+                <input type="text" name="ciudad" id="ciudad" placeholder="Ciudad (opcional)">
+
+                <input type="submit" value="Iniciar sesión" id="iniciar-sesion">
+
+                <div class="condiciones-container">
+                    <input type="checkbox" name="condiciones" id="condicicones">
+                    <p>Acepto los &nbsp<a href="#">términos y condiciones</a></p> 
+                </div>
+
+            </form>
+        </div>
+
+</div>
